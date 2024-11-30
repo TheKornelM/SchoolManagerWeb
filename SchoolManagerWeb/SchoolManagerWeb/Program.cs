760e4fe33bfb7686ef64067cbd11c4dadb706631
TheKornelM <makari.kornel@gmail.com>
2024-11-30 14:00:03 +0100

    Update preview packages to final version

--- SchoolManagerWeb/SchoolManagerWeb/Program.cs
+++ SchoolManagerWeb/SchoolManagerWeb/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SchoolManagerWeb.Client.Pages;
 using SchoolManagerWeb.Components;
 using SchoolManagerWeb.Components.Account;
 using SchoolManagerWeb.Data;
